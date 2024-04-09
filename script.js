da3834168bdaf630be5ee43ac655ff120670df2d
Rayza-18 <157272987+Rayza-18@users.noreply.github.com>
2024-04-09 11:56:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var guardarsenha = document.getElementById("#input_login_senha");
+var guardarEmail = document.getElementById("#input_login_email");
+
+function submit(){
+  guardarEmail = input_login_email.value;
+  guardarNome = input_login_senha.value;
+};
